3c731a8511bf629273dea33687d53d11efd8021a
Faraz Saifullah <isaifullah01@gmail.com>
2020-07-15 04:15:38 +0530

    Initial backend project setup with database integration, routes, controllers and dbConnectors defined

---
+++ backend/dbConnector/products.js
@@ -0,0 +1,41 @@
+"use strict";
+let DataService = require("../utils/datasource/DataService");
+
+class ProductsDbConnector {
+  constructor() {
+    this.dataService = new DataService();
+  }
+
+  async addNewProduct(body) {
+    const sqlQuery = {
+      text: `INSERT INTO products (name, stock, price, short_desc, description) 
+        VALUES ($1, $2, $3, $4, $5)`,
+      values: [
+        body.name,
+        body.stock,
+        body.price,
+        body.shortDesc,
+        body.description,
+      ],
+    };
+    try {
+      return await this.dataService.executeQueryAsPromise(sqlQuery, true);
+    } catch (err) {
+      return err;
+    }
+  }
+
+  async getAllProducts() {
+    const sqlQuery = {
+      text:
+        "SELECT * FROM products",
+    };
+    try {
+      return await this.dataService.executeQueryAsPromise(sqlQuery);
+    } catch (err) {
+      return err;
+    }
+  }
+}
+
+module.exports = ProductsDbConnector;
---
+++ backend/utils/APIResponseHandler/APIResponseHandler.js
@@ -0,0 +1,26 @@
+let ResponseCodes = require("../web/HttpResponseCodes");
+let HTTPResponseCodes = new ResponseCodes();
+
+class APIResponseHandler {
+  handle(response, result) {
+    if (result.success) {
+      return response.status(result.status).send(result);
+    } else {
+      return response
+        .status(HTTPResponseCodes.INTERNAL_SERVER_ERROR())
+        .send(result);
+    }
+  }
+
+  handleAuthentication(response, authenticationResponse) {
+    if (!authenticationResponse.success) {
+      return response.status(400).json(authenticationResponse);
+    } else if (authenticationResponse.error) {
+      return response.status(500).send(authenticationResponse);
+    } else {
+      return response.status(200).json(authenticationResponse);
+    }
+  }
+}
+
+module.exports = APIResponseHandler;
