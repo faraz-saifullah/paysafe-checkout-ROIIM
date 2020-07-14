917b650c0b3dce3efa13930b6fadb03065871aea
Faraz Saifullah <isaifullah01@gmail.com>
2020-07-14 23:56:28 +0530

    Initial React project setup with basic e-commerce like frontend

---
+++ src/App.js
@@ -0,0 +1,126 @@
+import React, { Component } from "react";
+import { Switch, Route, BrowserRouter as Router } from "react-router-dom";
+import Login from "./components/Login";
+import ProductList from "./components/products/ProductList";
+import AddProduct from "./components/addProduct/AddProduct";
+import Cart from "./components/cart/Cart";
+import data from "./Data";
+import Context from "./Context";
+import Navbar from "./components/navbar/Navbar";
+
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      user: null,
+      cart: {},
+      products: [],
+    };
+
+    this.routerRef = React.createRef();
+  }
+  login = (usn, pwd) => {
+    let user = data.users.find((u) => u.username === usn && u.password === pwd);
+    if (user) {
+      this.setState({ user });
+      localStorage.setItem("user", JSON.stringify(user));
+      return true;
+    }
+    return false;
+  };
+
+  logout = (e) => {
+    e.preventDefault();
+    this.setState({ user: null });
+    localStorage.removeItem("user");
+  };
+
+  addProduct = (product, callback) => {
+    let products = this.state.products.slice();
+    products.push(product);
+    localStorage.setItem("products", JSON.stringify(products));
+    this.setState({ products }, () => callback && callback());
+  };
+
+  addToCart = (cartItem) => {
+    let cart = this.state.cart;
+    if (cart[cartItem.id]) {
+      cart[cartItem.id].amount += cartItem.amount;
+    } else {
+      cart[cartItem.id] = cartItem;
+    }
+    if (cart[cartItem.id].amount > cart[cartItem.id].product.stock) {
+      cart[cartItem.id].amount = cart[cartItem.id].product.stock;
+    }
+    localStorage.setItem("cart", JSON.stringify(cart));
+    this.setState({ cart });
+  };
+
+  checkout = () => {
+    if (!this.state.user) {
+      this.routerRef.current.history.push("/login");
+      return;
+    }
+    const cart = this.state.cart;
+    const products = this.state.products.map((p) => {
+      if (cart[p.name]) {
+        p.stock = p.stock - cart[p.name].amount;
+      }
+      return p;
+    });
+    this.setState({ products });
+    this.clearCart();
+  };
+
+  removeFromCart = (cartItemId) => {
+    let cart = this.state.cart;
+    delete cart[cartItemId];
+    localStorage.setItem("cart", JSON.stringify(cart));
+    this.setState({ cart });
+  };
+
+  clearCart = () => {
+    let cart = {};
+    localStorage.setItem("cart", JSON.stringify(cart));
+    this.setState({ cart });
+  };
+
+  componentDidMount() {
+    let products = localStorage.getItem("products");
+    let cart = localStorage.getItem("cart");
+    let user = localStorage.getItem("user");
+    products = products ? JSON.parse(products) : data.initProducts;
+    cart = cart ? JSON.parse(cart) : {};
+    user = user ? JSON.parse(user) : null;
+    this.setState({ products, user, cart });
+  }
+
+  render() {
+    return (
+      <Context.Provider
+        value={{
+          ...this.state,
+          removeFromCart: this.removeFromCart,
+          addToCart: this.addToCart,
+          login: this.login,
+          addProduct: this.addProduct,
+          clearCart: this.clearCart,
+          checkout: this.checkout,
+        }}
+      >
+        <Router ref={this.routerRef}>
+          <div className="App">
+            <Navbar />
+            <Switch>
+              <Route exact path="/" component={ProductList} />
+              <Route exact path="/login" component={Login} />
+              <Route exact path="/cart" component={Cart} />
+              <Route exact path="/add-product" component={AddProduct} />
+              <Route exact path="/products" component={ProductList} />
+            </Switch>
+          </div>
+        </Router>
+      </Context.Provider>
+    );
+  }
+}
---
+++ src/components/products/ProductItem.js
@@ -0,0 +1,51 @@
+import React, { Component } from "react";
+
+class ProductItem extends Component {
+  render() {
+    const { product } = this.props;
+    return (
+      <div className=" column is-half">
+        <div className="box">
+          <div className="media">
+            <div className="media-left">
+              <figure className="image is-64x64">
+                <img
+                  src="https://bulma.io/images/placeholders/128x128.png"
+                  alt="product"
+                />
+              </figure>
+            </div>
+            <div className="media-content">
+              <b style={{ textTransform: "capitalize" }}>
+                {product.name}{" "}
+                <span className="tag is-primary">${product.price}</span>
+              </b>
+              <div>{product.shortDesc}</div>
+
+              {product.stock > 0 ? (
+                <small>{product.stock + " Available"}</small>
+              ) : (
+                <small className="has-text-danger">Out Of Stock</small>
+              )}
+              <div className="is-clearfix">
+                <button
+                  className="button is-small is-outlined is-primary   is-pulled-right"
+                  onClick={() =>
+                    this.props.addToCart({
+                      id: product.name,
+                      product,
+                      amount: 1,
+                    })
+                  }
+                >
+                  Add to Cart
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+export default ProductItem;
