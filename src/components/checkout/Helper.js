766145680a8e8971a05606bc215eb446714fe0ff
Faraz Saifullah <isaifullah01@gmail.com>
2020-07-16 08:25:24 +0530

    Accepting user data for non registered users, disabling process button while processing

--- src/components/checkout/Helper.js
+++ src/components/checkout/Helper.js
@@ -3,7 +3,7 @@ import axios from "axios";
 
 export default class Helper {
   constructor() {
-    this.merchantRefNum = '';
+    this.merchantRefNum = "";
     this.paymentResponse = {};
     this.paymentStatus = new Promise((resolve, reject) => {
       this.promiseResolve = resolve;
@@ -11,24 +11,19 @@ export default class Helper {
     });
   }
 
-  prepareSetupInput = (formInput, totalAmout) => {
+  prepareSetupInput = (billingAddress, customerInfo, totalAmout) => {
     this.merchantRefNum = Math.random().toString(36).slice(5);
+    customerInfo.dateOfBirth = {
+      day: Number(customerInfo.day),
+      month: Number(customerInfo.month),
+      year: Number(customerInfo.year),
+    };
     return {
       currency: `${config.currency}`,
-      amount: totalAmout * 100,
+      amount: totalAmout,
       locale: `${config.locale}`,
-      customer: {
-        firstName: "John",
-        lastName: "Dee",
-        email: "johndee@paysafe.com",
-        phone: "1234567890",
-        dateOfBirth: {
-          day: 1,
-          month: 7,
-          year: 1990,
-        },
-      },
-      billingAddress: formInput,
+      customer: customerInfo,
+      billingAddress: billingAddress,
       environment: `${config.environment}`,
       merchantRefNum: `${this.merchantRefNum}`,
       canEditAmount: false,
@@ -36,7 +31,7 @@ export default class Helper {
       displayPaymentMethods: ["card"],
       paymentMethodDetails: {},
     };
-  }
+  };
 
   callBackFunction = async (instance, error, result) => {
     if (result && result.paymentHandleToken) {
@@ -47,34 +42,34 @@ export default class Helper {
           amount: result.amount,
         });
         instance.showSuccessScreen(`Payment ID: ${response.data.id}`);
-        this.paymentResponse.status = "success"
+        this.paymentResponse.status = "success";
         this.paymentResponse.message = "Payment Successfully Processed!";
         this.promiseResolve(this.paymentResponse);
       } catch (err) {
         instance.showFailureScreen("Payment Failed! Please Try Again.");
-        this.paymentResponse.status = "failed"
+        this.paymentResponse.status = "failed";
         this.paymentResponse.message = "Unable To Process Payment";
         this.promiseReject(this.paymentResponse);
         throw err;
       }
     } else {
-      this.paymentResponse.status = "failed"
+      this.paymentResponse.status = "failed";
       this.paymentResponse.message = "Unable To Initialize Payment";
       this.promiseReject(this.paymentResponse);
       throw error;
     }
-  }
+  };
 
   closeCallBack = (stage, expired) => {
     if (expired) {
-      this.paymentResponse.status = "failed"
+      this.paymentResponse.status = "failed";
       this.paymentResponse.message = "Session Expired";
       this.promiseReject(this.paymentResponse);
     } else if (this.paymentResponse.status !== "success") {
-      this.paymentResponse.status = "failed"
+      this.paymentResponse.status = "failed";
       this.paymentResponse.message = "Payment Closed Unexpectedly By The User!";
       this.promiseReject(this.paymentResponse);
     }
     this.promiseResolve();
-  }
+  };
 }
