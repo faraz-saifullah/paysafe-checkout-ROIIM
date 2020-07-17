713bb016213dca5b9dde4a3d53aa015bf37e7c66
Faraz Saifullah <isaifullah01@gmail.com>
2020-07-17 21:47:49 +0530

    Error logging

--- backend/routes/payment.js
+++ backend/routes/payment.js
@@ -5,6 +5,11 @@ let APIResponseHandler = require("../utils/APIResponseHandler/APIResponseHandler
 
 router.post("/", async function (req, res, next) {
   let result = await new Payment().processPayment(req);
+  console.log('Payment Request:', req.body);
+  if (result.data)
+    console.log('Payment Processing Response', result.data);
+  else
+    console.log('Payment Processing Error Response', result);
   return new APIResponseHandler().handlePaysafe(res, result);
 });
 
