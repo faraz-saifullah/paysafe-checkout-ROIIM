f6b9790ecfcb14e691b14368732e6f49c09dba3a
Faraz Saifullah <isaifullah01@gmail.com>
2020-07-17 21:47:49 +0530

    Error logging

--- backend/routes/token.js
+++ backend/routes/token.js
@@ -5,9 +5,17 @@ let APIResponseHandler = require("../utils/APIResponseHandler/APIResponseHandler
 
 router.post("/", async function (req, res, next) {
   let result = await new Token().createSingleUseToken(req);
+<<<<<<< HEAD
   console.log("Token Creation Request: ", req.body);
   if (result.data) console.log("Token Creation Response", result.data);
   else console.error("Token Creation Error Response", result);
+=======
+  console.log('Token Creation Request: ', req.body);
+  if (result.data)
+    console.log('Token Creation Response', result.data);
+  else
+    console.log('Token Creation Error Response', result);
+>>>>>>> 713bb01... Error logging
   return new APIResponseHandler().handlePaysafe(res, result);
 });
 
