82a8625c05cb6e289ccfacfd53061b2ac8cdb907
Faraz Saifullah <isaifullah01@gmail.com>
2020-07-18 02:25:43 +0530

    Checkout form input validation

--- backend/routes/payment.js
+++ backend/routes/payment.js
@@ -5,17 +5,9 @@ let APIResponseHandler = require("../utils/APIResponseHandler/APIResponseHandler
 
 router.post("/", async function (req, res, next) {
   let result = await new Payment().processPayment(req);
-<<<<<<< HEAD
   console.log("Payment Request:", req.body);
   if (result.data) console.log("Payment Processing Response", result.data);
   else console.error("Payment Processing Error Response", result);
-=======
-  console.log('Payment Request:', req.body);
-  if (result.data)
-    console.log('Payment Processing Response', result.data);
-  else
-    console.log('Payment Processing Error Response', result);
->>>>>>> 713bb01... Error logging
   return new APIResponseHandler().handlePaysafe(res, result);
 });
 
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -5,17 +5,9 @@ const APIResponseHandler = require("../utils/APIResponseHandler/APIResponseHandl
 
 router.post("/", async function (req, res) {
   let result = await new User().addUser(req);
-<<<<<<< HEAD
   console.log("User Creation Request", req.body);
   if (result.data) console.log("User Creation Response", result.data);
   else console.error("User Creation Error Response", result);
-=======
-  console.log('User Creation Request', req.body);
-  if (result.data)
-    console.log('User Creation Response', result.data);
-  else
-    console.log('User Creation Error Response', result);
->>>>>>> 713bb01... Error logging
   return new APIResponseHandler().handlePaysafe(res, result);
 });
 
